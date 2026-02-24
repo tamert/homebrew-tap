4353314ae39ba76a449681976fcfb4bfffaa985a
Tamer Ağaoğlu <tamer.a@90pixel.com>
2026-02-24 15:32:33 +0300

    Use copysync.tr static URL for cask download

--- Casks/copysync.rb
+++ Casks/copysync.rb
@@ -2,7 +2,7 @@ cask "copysync" do
   version "2.0.6"
   sha256 "068bcfb6d7426ee64e60dae049c2b7a5e9cec961b18189ae07d96f61c950a9d8"
 
-  url "https://github.com/tamert/copy-sync/releases/download/v2.0.6/CopySync.zip"
+  url "https://copysync.tr/static/CopySync.zip"
   name "CopySync"
   desc "Multi-platform clipboard synchronization"
   homepage "https://copysync.tr"
